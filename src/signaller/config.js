43842a0c03f813fca834c11312f662318083d1c4
root <root@ip-172-31-45-230.ap-south-1.compute.internal>
2024-03-13 16:40:33 +0000

    updated config.js

--- src/signaller/config.js
+++ src/signaller/config.js
@@ -1,8 +1,8 @@
 module.exports = {
   listenIp: '0.0.0.0',
   listenPort: 443,
-  sslCrt: './cert.pem',
-  sslKey: './key.pem',
+  sslCrt: '/var/tmp/key/certificate.crt',
+  sslKey: '/var/tmp/key/private_key.pem',
 
   
 };
