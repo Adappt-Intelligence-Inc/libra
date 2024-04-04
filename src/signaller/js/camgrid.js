6f1b4100814152020b73191db0cada99414db50e
Arvind Umrao <akumrao@yahoo.com>
2024-04-04 15:30:59 +0530

    changed the test feed

--- src/signaller/js/camgrid.js
+++ src/signaller/js/camgrid.js
@@ -763,7 +763,7 @@ function test()
 {
 
      var divAdd  =  document.getElementById("liveS11").children[0];
-     addCamera("65c108570948a0346f67424623c38f86a7e718712aceadb10ac867", divAdd);
+     addCamera("65f570720af337cec5335a70ee88cbfb7df32b5ee33ed0b4a896a0", divAdd);
 
 }
 
