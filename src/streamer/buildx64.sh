a18c0c08a665b1feaed97b7a02f697e676bdb8f2
Arvind Umrao <akumrao@yahoo.com>
2024-07-20 13:50:47 +0530

    buildx64.sh

--- src/streamer/buildx64.sh
+++ src/streamer/buildx64.sh
@@ -1,9 +1,8 @@
-rm -rf build
-mkdir build
-
 git clean -xfd
 
-cd build 
+rm -rf ./build
+mkdir ./build
+cd ./build 
 
 cmake .. -DUSE_OPENSSL=OFF -DUSE_MBEDTLS=ON -DBUILD_STATIC_LIBS=ON  -DBUILD_LIBSRTP_DESTINATION_PLATFORM=mips-linux-gnu -DBOARD=FILE -DCMAKE_BUILD_TYPE=Debug --log-level=VERBOSE
 
